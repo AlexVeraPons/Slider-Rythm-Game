9bf5f7454e545a592e4caadf491ba7059cc80f8e
Alexandro Vera <Alexandroverapons@gmail.com>
2023-01-18 21:31:37 +0100

    Working on ArrowManager.cs and Arrow.cs

--- Assets/_Script/Arrow.cs
+++ Assets/_Script/Arrow.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    // TODO 
+    // check if an arrow is entering your score area 
+    // check for swipes 
+    // calculate score
+    
     // Start is called before the first frame update
+    
+    private Transform _transform;
     public enum ArrowType
     {
         Up,
@@ -17,16 +24,19 @@ public class Arrow : MonoBehaviour
         DownLeft
     }
 
-    public ArrowType _arrowType;
+    public ArrowType ArrowDirection { get; set; }
+
+    private float _xPos { get; set; }
+    private float _yPos { get; set; }
 
     void Start()
     {
-        
+        _transform = this.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _transform.position = new Vector2(_xPos, _yPos);
     }
 }
--- Assets/_Script/ArrowManager.cs
+++ Assets/_Script/ArrowManager.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class ArrowManager : MonoBehaviour
 {
+    //Todo
+    // arrow position
+    // arrow spawner or have an arrow spawner
+
     // Start is called before the first frame update
+    private Arrow[] _arrowList;
+    [SerializeField] private GameObject _arrows;
+
+    private float _screenWidth;
+    private float _screenHeight;
+
     void Start()
-    {
-        
+    {   
+        // fill the arrow list with the children of the arrows object
+       for (int i = 0; i < _arrows.GetComponent<Transform>().childCount; i++)
+        {
+            _arrowList[i] = _arrows.GetComponent<Transform>().GetChild(i).gameObject.GetComponent<Arrow>();
+        }
+
+        // get the screen width and height
+        _screenWidth = Screen.width;
+        _screenHeight = Screen.height;
+
+        // set the arrow position
+        ArrowPositionSetter();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void ArrowPositionSetter()
+    {
+        // set the position of the arrows
+        for (int i = 0; i < _arrowList.Length; i++)
+        {
+            switch (_arrowList[i].ArrowDirection)
+            {
+                case Arrow.ArrowType.Up:
+                    break; 
+                case Arrow.ArrowType.Down:
+                    break;
+                case Arrow.ArrowType.Left:
+                    break;
+                case Arrow.ArrowType.Right:
+                    break;
+                case Arrow.ArrowType.UpRight:
+                    break;
+                case Arrow.ArrowType.UpLeft:
+                    break;
+                case Arrow.ArrowType.DownRight:
+                    break;
+                case Arrow.ArrowType.DownLeft:
+                    break;
+                default:
+                    break;  
+            }
+        }
     }
 }
